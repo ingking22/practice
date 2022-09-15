c7cb15feb60c29fa099e92076e9b0e93dfc3090d
ingking22 <ybj1988@nate.com>
2022-09-15 11:10:30 +0900

    Initial commit

---
+++ src/main/java/com/example/practice/controller/main.java
@@ -0,0 +1,15 @@
+package com.example.practice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class main {
+
+
+    @GetMapping("/main")
+    public String main(){
+        return "Hello World";
+    }
+
+}
