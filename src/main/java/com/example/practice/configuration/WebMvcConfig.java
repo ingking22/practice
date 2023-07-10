04d4e879ae3223f6c7331f3eb0f61b5931a0a71a
ingking22 <ybj1988@nate.com>
2023-07-10 17:52:44 +0900

    Changes

---
+++ src/main/java/com/example/practice/configuration/WebMvcConfig.java
@@ -0,0 +1,5 @@
+package com.example.practice.configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer  {
+}
